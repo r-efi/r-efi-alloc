6d3f05e659f866614fa6026c17984754c00a8a69
David Rheinsberg <david.rheinsberg@gmail.com>
2022-11-25 13:30:14 +0100

    alloc: reformat and improve docs
    Reformat the code to align with our coding-style and avoid overlong
    lines. Furthermore, reformat the documentation to adhere as well, and
    extend it were possible.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,28 +1,33 @@
 //! UEFI Memory Allocators
 //!
-//! This module provides a memory allocator that integrates with the UEFI pool allocator. It
-//! exports an `Allocator` type that wraps a System-Table together with a UEFI memory type and
-//! forwards memory requests to the UEFI pool allocator.
+//! This module provides a memory allocator that integrates with the UEFI pool
+//! allocator. It exports an `Allocator` type that wraps a System-Table
+//! together with a UEFI memory type and forwards memory requests to the UEFI
+//! pool allocator.
 //!
-//! The allocator implements the `core::alloc::Allocator` API defined by the rust standard library.
-//! Apart from the constructors, no private extensions are defined. For documentation on the
-//! allocation-API, see the rust standard library.
+//! The allocator implements the `core::alloc::Allocator` API defined by the
+//! rust standard library. Apart from the constructors, no private extensions
+//! are defined. For documentation on the allocation-API, see the rust standard
+//! library.
 //!
-//! Note that `core::alloc::Allocator` is marked unstable as of time of this crate-release. That
-//! is, future versions of this trait definition might be incompatible to the current version.
-//! Make sure you use a crate-version that matches your standard-library.
+//! Note that `core::alloc::Allocator` is marked unstable as of time of this
+//! crate-release. That is, future versions of this trait definition might be
+//! incompatible to the current version. Make sure you use a crate-version that
+//! matches your standard-library.
 
 use r_efi::efi;
 
 /// Memory Allocator
 ///
-/// This crate implements a rust memory allocator that forwards requests to the UEFI pool
-/// allocator. It takes a System-Table as input, as well as the memory type to use as backing, and
-/// then forwards all memory allocation requests to the `AllocatePool()` UEFI system.
+/// This crate implements a rust memory allocator that forwards requests to the
+/// UEFI pool allocator. It takes a System-Table as input, as well as the
+/// memory type to use as backing, and then forwards all memory allocation
+/// requests to the `AllocatePool()` UEFI system.
 ///
-/// The `core::alloc::Allocator` trait is implemented for this allocator. Hence, this allocator can
-/// also be used to back the global memory-allocator of `liballoc` (or `libstd`). See the `Global`
-/// type for an implementation of the global allocator.
+/// The `core::alloc::Allocator` trait is implemented for this allocator.
+/// Hence, this allocator can also be used to back the global memory-allocator
+/// of `liballoc` (or `libstd`). See the `Global` type for an implementation of
+/// the global allocator.
 pub struct Allocator {
     system_table: *mut efi::SystemTable,
     memory_type: efi::MemoryType,
@@ -31,16 +36,19 @@ pub struct Allocator {
 impl Allocator {
     /// Create Allocator from UEFI System-Table
     ///
-    /// This creates a new Allocator object from a UEFI System-Table pointer and the memory-type
-    /// to use for allocations. That is, all allocations on this object will be tunnelled through
-    /// the `AllocatePool` API on the given System-Table. Allocations will always use the memory
-    /// type given as @memtype.
+    /// This creates a new Allocator object from a UEFI System-Table pointer
+    /// and the memory-type to use for allocations. That is, all allocations on
+    /// this object will be tunnelled through the `AllocatePool` API on the
+    /// given System-Table. Allocations will always use the memory type given
+    /// as `memtype`.
     ///
-    /// Note that this interface is unsafe, since the caller must guarantee that the System-Table
-    /// is valid for as long as the Allocator is. Furthermore, the caller must guarantee validity
-    /// of the system-table-interface. The latter is usually guaranteed by the provider of the
-    /// System-Table. The former is usually just a matter of tearing down the allocator before
-    /// returning from your application entry-point.
+    /// Note that this interface is unsafe, since the caller must guarantee
+    /// that the System-Table is valid for as long as the Allocator is.
+    /// Furthermore, the caller must guarantee validity of the
+    /// system-table-interface. The latter is usually guaranteed by the
+    /// provider of the System-Table. The former is usually just a matter of
+    /// tearing down the allocator before returning from your application
+    /// entry-point.
     pub unsafe fn from_system_table(
         st: *mut efi::SystemTable,
         memtype: efi::MemoryType,
@@ -68,7 +76,9 @@ unsafe impl core::alloc::Allocator for Allocator {
         }
 
         let ptr = if size > 0 {
-            unsafe { crate::raw::alloc(self.system_table, layout, self.memory_type) }
+            unsafe {
+                crate::raw::alloc(self.system_table, layout, self.memory_type)
+            }
         } else {
             layout.dangling().as_ptr() as *mut _
         };
@@ -77,13 +87,18 @@ unsafe impl core::alloc::Allocator for Allocator {
             Err(core::alloc::AllocError)
         } else {
             Ok(
-                core::ptr::NonNull::new(core::ptr::slice_from_raw_parts(ptr, size) as *mut _)
-                    .unwrap(),
+                core::ptr::NonNull::new(
+                    core::ptr::slice_from_raw_parts(ptr, size) as *mut _,
+                ).unwrap(),
             )
         }
     }
 
-    unsafe fn deallocate(&self, ptr: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
+    unsafe fn deallocate(
+        &self,
+        ptr: core::ptr::NonNull<u8>,
+        layout: core::alloc::Layout,
+    ) {
         if layout.size() != 0 {
             crate::raw::dealloc(self.system_table, ptr.as_ptr(), layout)
         }
